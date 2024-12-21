76fac261392bb6c4cc61990c8cc051a4b35d350d
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-21 13:41:23 -0500

    removed export from scripts but added to netlify.config file. Also updated netlify.toml file.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enable static export
+  reactStrictMode: true,
+};
 
 export default nextConfig;
