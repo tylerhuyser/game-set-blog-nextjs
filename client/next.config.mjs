7417ec0fb4ec69c53a24b7f23d503729b278edbd
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-22 13:21:55 -0500

    added regex to handle redirect in next.config and prevent infinte loop.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,21 +4,21 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/posts/:id/:slug',
+        source: '/posts/:id(\\d{1,3})/:slug', // Match only numeric IDs
         destination: '/posts/:slug',
-        permanent: true
+        permanent: true,
       },
       {
-        source: '/:id/:slug',
+        source: '/:id(\\d{1,3})/:slug', // Match only numeric IDs outside "posts"
         destination: '/posts/:slug',
-        permanent: true
+        permanent: true,
       },
       {
-        source: '/:year/:month/:date/:slug',
+        source: '/:year(\\d{4})/:month(\\d{2})/:date(\\d{2})/:slug', // Match dates
         destination: '/posts/:slug',
-        permanent: true
+        permanent: true,
       },
-    ]
+    ];
   }
 };
 
