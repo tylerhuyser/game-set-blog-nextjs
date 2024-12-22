f1c40453624903b32125e21fd14962934153d105
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-22 13:11:13 -0500

    revised reidercts file, netlify.toml and added frontend redierct parameters to netlify.config file.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/posts/:id/:slug',
+        destination: '/posts/:slug',
+        permanent: true
+      },
+      {
+        source: '/:id/:slug',
+        destination: '/posts/:slug',
+        permanent: true
+      },
+      {
+        source: '/:year/:month/:date/:slug',
+        destination: '/posts/:slug',
+        permanent: true
+      },
+    ]
+  }
 };
 
 export default nextConfig;
